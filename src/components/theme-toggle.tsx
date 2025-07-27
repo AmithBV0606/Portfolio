2c78b1fccffa9c6162eaed646999b62d3603160c
Amith B V <amithrao0606@gmail.com>
2025-07-27 14:44:36 +0530

    Added theme-animation component from Skiper-UI and used it  in theme-toggle button.

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,24 +1,97 @@
 "use client";
 
-import * as React from "react";
+import React from "react";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
 
-export function ThemeToggle() {
+import {
+  AnimationStart,
+  AnimationVariant,
+  createAnimation,
+} from "@/components/ui/theme-animations";
+
+interface ThemeToggleAnimationProps {
+  variant?: AnimationVariant;
+  start?: AnimationStart;
+  showLabel?: boolean;
+  url?: string;
+}
+
+export default function ThemeToggle({
+  variant = "circle-blur",
+  start = "top-left",
+  showLabel = false,
+  url = "",
+}: ThemeToggleAnimationProps) {
   const { theme, setTheme } = useTheme();
-  console.log(theme);
+
+  const styleId = "theme-transition-styles";
+
+  const updateStyles = React.useCallback((css: string, name: string) => {
+    if (typeof window === "undefined") return;
+
+    let styleElement = document.getElementById(styleId) as HTMLStyleElement;
+
+    console.log("style ELement", styleElement);
+    console.log("name", name);
+
+    if (!styleElement) {
+      styleElement = document.createElement("style");
+      styleElement.id = styleId;
+      document.head.appendChild(styleElement);
+    }
+
+    styleElement.textContent = css;
+
+    console.log("content updated");
+  }, []);
+
+  const toggleTheme = React.useCallback(() => {
+    const animation = createAnimation(variant, start, url);
+
+    updateStyles(animation.css, animation.name);
+
+    if (typeof window === "undefined") return;
+
+    const switchTheme = () => {
+      setTheme(theme === "light" ? "dark" : "light");
+    };
+
+    if (!document.startViewTransition) {
+      switchTheme();
+      return;
+    }
+
+    document.startViewTransition(switchTheme);
+  }, [theme, setTheme]);
 
   return (
     <Button
-      variant="outline"
+      onClick={toggleTheme}
+      variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="cursor-pointer rounded-full border-0 bg-[#ffdfb0] hover:bg-[#f7ddb7] dark:bg-[#393128] hover:dark:bg-[#393128]"
+      className="w-9 p-0 h-9 relative group cursor-pointer rounded-full border-0 bg-[#ffdfb0] hover:bg-[#f7ddb7] dark:bg-[#393128] hover:dark:bg-[#393128]"
+      name="Theme Toggle Button"
     >
-      <SunIcon className="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 text-[#D27D2D]" />
-      <MoonIcon className="absolute size-4 scale-0 rotate-0 transition-all dark:scale-100 text-[#F3D8C7]" />
+      <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+
+      <MoonIcon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+
+      <span className="sr-only">Theme Toggle </span>
+      {showLabel && (
+        <>
+          <span className="hidden group-hover:block border rounded-full px-2 absolute -top-10">
+            {" "}
+            variant = {variant}
+          </span>
+          <span className="hidden group-hover:block border rounded-full px-2 absolute -bottom-10">
+            {" "}
+            start = {start}
+          </span>
+        </>
+      )}
     </Button>
   );
 }
