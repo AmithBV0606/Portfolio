85ef5e67dfa4df9b5c5ed89e3c82ede451fd6f50
Amith B V <amithrao0606@gmail.com>
2025-07-27 14:46:00 +0530

    Created a general purpose link component using "Link" of "next/link".

---
+++ src/components/ui/link.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+import Link from 'next/link';
+
+interface Props {
+  href: string;
+  external?: boolean;
+  className?: string;
+  underline?: boolean;
+  [key: string]: any;
+}
+
+const LinkComponent: React.FC<Props> = ({ href, external, className, underline, children, ...rest }) => {
+  return (
+    <Link
+      href={href}
+      target={external ? '_blank' : '_self'}
+      className={cn(
+        'inline-block transition-colors duration-300 ease-in-out',
+        underline &&
+          'underline decoration-muted-foreground underline-offset-[3px] hover:decoration-foreground',
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default Link;
