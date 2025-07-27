f6697f614a31c44f7a0c2335becf07e8a2baf68b
Amith B V <amithrao0606@gmail.com>
2025-07-27 15:18:24 +0530

    Created a new Navbar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Navbar />
-      <div className="w-full text-center mt-10">
+
+      {/* <div className="w-full text-center mt-10">
         <p className="text-2xl font-bold underline">
           Welcome to HomePage!!
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
