842ea8706a49ec0ed68b6858ab2126f425ae7bde
Amith B V <amithrao0606@gmail.com>
2025-07-29 12:39:51 +0530

    Moved the Navbar and Footer components to latout.tsx file so that it's available for other pages as well.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
+import FooterSection from "@/components/FooterSection";
 
 const clashDisplay = localFont({
   src: [
     {
-      path: './fonts/ClashDisplay-Semibold.eot',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/ClashDisplay-Semibold.eot",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/ClashDisplay-Semibold.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/ClashDisplay-Semibold.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/ClashDisplay-Semibold.woff',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/ClashDisplay-Semibold.woff",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/ClashDisplay-Semibold.woff2',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/ClashDisplay-Semibold.woff2",
+      weight: "400",
+      style: "normal",
     },
   ],
-})
+});
 
 export const metadata: Metadata = {
   title: "HeyAmith",
@@ -40,17 +42,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={clashDisplay.className}
-      >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+      <body className={clashDisplay.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+
+          {children}
+
+          <FooterSection />
+        </ThemeProvider>
       </body>
     </html>
   );
