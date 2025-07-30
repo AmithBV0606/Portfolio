c480756f624ae92e9fc9f13da1b2918cb8198926
Amith B V <amithrao0606@gmail.com>
2025-07-30 08:11:19 +0530

    Adjusted the UI of not-found page according to the different screen sizes.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { buttonVariants } from "@/components/ui/button";
 import FuzzyText from "@/components/ui/FuzzyText";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
+import { useTheme } from "next-themes";
 
 export default function NotFound() {
+  const { theme } = useTheme();
   return (
-    <div className="h-screen w-full flex justify-center items-center">
-      <section className="flex w-full flex-col items-center justify-center gap-y-4 text-center">
+    <div className="relative container mx-2 md:mx-auto top-80 md:top-72 sm:px-8 lg:px-16">
+      <section className="flex flex-col items-center justify-center gap-y-4 text-center">
         <div className="flex flex-col items-center justify-center gap-4">
           <FuzzyText
             baseIntensity={0.2}
@@ -15,6 +19,7 @@ export default function NotFound() {
             enableHover={true}
             fontSize={150}
             fontWeight={"950"}
+            color={theme === "dark" ? "#fff" : "#000"}
           >
             404
           </FuzzyText>
@@ -24,11 +29,12 @@ export default function NotFound() {
             hoverIntensity={0.5}
             enableHover={true}
             fontSize={40}
+            color={theme === "dark" ? "#fff" : "#000"}
           >
             Page Not Found!
           </FuzzyText>
 
-          <p className="prose max-w-96 text-sm text-gray-400">
+          <p className="prose max-w-72 md:max-w-96 text-sm text-justify text-gray-500 dark:text-gray-400">
             Oops! The page you're looking for doesn't exist. It might have been
             moved, deleted, or you entered the wrong URL.
           </p>
