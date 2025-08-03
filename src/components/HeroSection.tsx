352c680852d1c82b77d11b7b58ade24b96d12556
Amith B V <amithrao0606@gmail.com>
2025-08-03 21:03:53 +0530

    Created a downloadable resume button for small screens and a "cmd + P" for desktop screens.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { SITE } from "@/constants/constants";
 import Image from "next/image";
+import ResumeDownloadCmdP from "./ResumeDownloadCmdP";
+import debounce from "lodash.debounce";
+import WrapButton from "@/components/skiperui/wrap-button";
+import { Globe } from "lucide-react";
 
 const HeroSection = () => {
+  const [open, setOpen] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
+      const isPrintShortcut =
+        (isMac && e.metaKey && e.key.toLowerCase() === "p") ||
+        (!isMac && e.ctrlKey && e.key.toLowerCase() === "p");
+
+      if (isPrintShortcut) {
+        e.preventDefault(); // Stop the print dialog
+        setOpen((prev) => !prev);
+      }
+    };
+
+    window.addEventListener("keydown", handler);
+    return () => window.removeEventListener("keydown", handler);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      const isMobileView = window.matchMedia("(max-width: 1024px)").matches;
+      setIsMobile(isMobileView);
+    }, 100);
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className="relative top-20 md:top-36 container mx-auto w-auto">
       <div className="flex flex-col items-center text-center sm:flex-row sm:items-start sm:gap-8 sm:text-left">
@@ -41,6 +80,22 @@ const HeroSection = () => {
           >
             {SITE.description}
           </p>
+
+          {/* Resume Download Options : */}
+          <div className="mt-3 xl:mt-4 ml-2">
+            {isMobile ? (
+              <div className="flex justify-center md:justify-start">
+                <WrapButton
+                  href="public/Amith-Resume.pdf"
+                >
+                  <Globe className="animate-spin " />
+                  Download Resume
+                </WrapButton>
+              </div>
+            ) : (
+              <ResumeDownloadCmdP open={open} setOpen={setOpen} />
+            )}
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/ResumeDownloadCmdP.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import * as React from "react";
+import {
+  CommandMenu,
+  CommandMenuTrigger,
+  CommandMenuContent,
+  useCommandMenuShortcut,
+} from "@/components/hextaui/command-menu";
+import { Button } from "@/components/ui/button";
+import { Kbd } from "@/components/hextaui/kbd";
+import { Download } from "lucide-react";
+import Image from "next/image";
+import { Separator } from "./ui/separator";
+import Link from "next/link";
+
+// Utility function to detect OS and return appropriate modifier key
+const getModifierKey = () => {
+  if (typeof navigator === "undefined") return { key: "Ctrl", symbol: "Ctrl" };
+
+  const isMac =
+    navigator.platform.toUpperCase().indexOf("MAC") >= 0 ||
+    navigator.userAgent.toUpperCase().indexOf("MAC") >= 0;
+
+  return isMac ? { key: "cmd", symbol: "⌘" } : { key: "ctrl", symbol: "Ctrl" };
+};
+
+const ResumeDownloadCmdP = ({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  useCommandMenuShortcut(() => setOpen(true));
+
+  return (
+    <CommandMenu open={open} onOpenChange={setOpen}>
+      <CommandMenuTrigger asChild>
+        <Button variant="outline" className="gap-2 cursor-pointer bg-red-500">
+          {/* <Command size={16} /> */}
+          Download Resume
+          <div className="ml-auto flex items-center gap-1">
+            <Kbd size="sm">{getModifierKey().symbol}</Kbd>
+            <h1>+</h1>
+            <Kbd size="sm">P</Kbd>
+          </div>
+        </Button>
+      </CommandMenuTrigger>
+
+      <CommandMenuContent
+        className="rounded-xl max-w-xl h-[90vh] mt-44 overflow-y-auto bg-[#141414]"
+        showShortcut={false}
+      >
+        <div>
+          <div className="p-2">
+            <h1 className="font-bold">Resume</h1>
+          </div>
+
+          <Separator className="bg-gray-700" />
+
+          <div className="p-2 h-[80%] flex justify-center items-center">
+            <Image
+              src={"/images/Resume.png"}
+              alt="Resume-Preview"
+              height={300}
+              width={500}
+              className="object-cover rounded-sm brightness-[0.8]"
+            />
+          </div>
+
+          <Separator className="bg-gray-700" />
+
+          <div className="flex gap-2 p-2 justify-end mt-2">
+            <Button
+              variant="outline"
+              onClick={() => setOpen(false)}
+              className="cursor-pointer"
+            >
+              Cancel
+            </Button>
+            <Button variant="default">
+              <Link
+                href={"/Amith-Resume.pdf"}
+                download={true}
+                className="flex cursor-pointer gap-2"
+              >
+                <span>Download</span>
+
+                <Download />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </CommandMenuContent>
+    </CommandMenu>
+  );
+};
+
+export default ResumeDownloadCmdP;
