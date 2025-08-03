02a12eb558e8fc5752d33cb5696c7557edea94b9
Amith B V <amithrao0606@gmail.com>
2025-08-03 17:46:25 +0530

    Replaced the rss icon and links with "twitter" icon i.e "X".

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,6 @@
 import type { IconMap, SocialLink, Site, Technologies, NavLink } from '@/types/index';
 import { Github, Mail, Instagram, Rss, LinkedinIcon } from "lucide-react";
+import { FaXTwitter } from "react-icons/fa6";
 
 export const SITE: Site = {
   title: 'Amith B V',
@@ -44,8 +45,8 @@ export const SOCIAL_LINKS: SocialLink[] = [
     label: LinkedinIcon,
   },
   {
-    href: '/rss.xml',
-    label: Rss,
+    href: 'https://x.com/heyyamith',
+    label: FaXTwitter,
   },
 ]
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,50 +1,51 @@
 import { LucideIcon } from "lucide-react";
+import { type IconType } from 'react-icons'
 
 export type Site = {
-title: string
-description: string
-href: string
-author: string
-locale: string
-location: string
+title: string;
+description: string;
+href: string;
+author: string;
+locale: string;
+location: string;
 }
 
 export type NavLink = {
-  href: string
-  label: string
+  href: string;
+  label: string;
 }
 
 export type SocialLink = {
-  href: string
-  label: LucideIcon
+  href: string;
+  label: LucideIcon | IconType;
 }
 
 export type IconMap = {
-  [key: string]: string
+  [key: string]: string;
 }
 
 export interface Category {
-  text: string
-  logo: string
+  text: string;
+  logo: string;
 }
 
 export type Technologies = {
-  'Frontend': Category[]
-  'Backend': Category[]
-  Databases: Category[]
-  'Package Managers': Category[],
-  'Deployment': Category[],
-  'DevOps': Category[],
-  'Development Tools': Category[],
-  'Operating Systems': Category[]
-  'Other Programming Languages and Technologies': Category[]
-  'Other Software': Category[]
+  'Frontend': Category[];
+  'Backend': Category[];
+  Databases: Category[];
+  'Package Managers': Category[];
+  'Deployment': Category[];
+  'DevOps': Category[];
+  'Development Tools': Category[];
+  'Operating Systems': Category[];
+  'Other Programming Languages and Technologies': Category[];
+  'Other Software': Category[];
 }
 
 export type BreadCrumbsProps = {
-  label: string, 
-  href?: string, 
-  icon?: LucideIcon
+  label: string; 
+  href?: string;
+  icon?: LucideIcon;
 }
 
 export type ProjectDataType = {
